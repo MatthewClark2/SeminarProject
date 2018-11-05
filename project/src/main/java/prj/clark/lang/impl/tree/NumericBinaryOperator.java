1fabbc80df7cfd76c60ddff2849b56ee56590b35
Matthew Clark <matthew.clark.dev@gmail.com>
2018-11-04 23:01:14 -0600

    Add small reminder to remove instanceof checks from the implementing classes of NumericBinaryOperator.

--- project/src/main/java/prj/clark/lang/impl/tree/NumericBinaryOperator.java
+++ project/src/main/java/prj/clark/lang/impl/tree/NumericBinaryOperator.java
@@ -6,6 +6,8 @@ import prj.clark.lang.impl.err.TypeMismatchException;
 
 import java.util.function.BiFunction;
 
+// TODO(matthew-c21) - Most implementing subclasses directly check to see if the value is a LangInt or LangFloat. Alter
+// them to see if the value is of type Int or type Float.
 abstract class NumericBinaryOperator extends BinaryOperator {
     protected NumericBinaryOperator(Node left, Node right, BiFunction<Data, Data, Data> op) {
         super(left, right, op);
