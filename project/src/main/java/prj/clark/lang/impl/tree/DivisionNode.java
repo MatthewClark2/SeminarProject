339809fd51b3fc5aba8241af5d9017f03e032f82
Matthew Clark <matthew.clark.dev@gmail.com>
2018-11-11 23:46:48 -0600

    Update DivisionNode to only return floating point numbers to make room for a possible integer float operator down the line.

--- project/src/main/java/prj/clark/lang/impl/tree/DivisionNode.java
+++ project/src/main/java/prj/clark/lang/impl/tree/DivisionNode.java
@@ -2,19 +2,14 @@ package prj.clark.lang.impl.tree;
 
 import prj.clark.lang.impl.env.Data;
 import prj.clark.lang.impl.env.LangFloat;
-import prj.clark.lang.impl.env.LangInt;
 
 import java.util.function.BiFunction;
 
 public class DivisionNode extends NumericBinaryOperator {
     public DivisionNode(Node left, Node right) {
-        super(left, right, (l,r) -> {
-            if (l instanceof LangFloat || r instanceof LangFloat) {
-                return LangFloat.of(Double.parseDouble(l.toString()) / Double.parseDouble(r.toString()));
-            } else {
-                return LangInt.of(Long.parseLong(l.toString()) / Long.parseLong(r.toString()));
-            }
-        });
+        super(left, right, (l,r) ->
+            LangFloat.of(Double.parseDouble(l.toString()) / Double.parseDouble(r.toString()))
+        );
     }
     protected DivisionNode(Node left, Node right, BiFunction<Data, Data, Data> op) {
         super(left, right, op);
