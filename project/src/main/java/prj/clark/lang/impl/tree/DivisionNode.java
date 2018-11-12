a800c34403c9f180186365a37cad98b36eb113d4
Matthew Clark <matthew.clark.dev@gmail.com>
2018-11-11 23:47:55 -0600

    Remove leftover protected constructor from DivisionNode.

--- project/src/main/java/prj/clark/lang/impl/tree/DivisionNode.java
+++ project/src/main/java/prj/clark/lang/impl/tree/DivisionNode.java
@@ -1,17 +1,11 @@
 package prj.clark.lang.impl.tree;
 
-import prj.clark.lang.impl.env.Data;
 import prj.clark.lang.impl.env.LangFloat;
-
-import java.util.function.BiFunction;
 
 public class DivisionNode extends NumericBinaryOperator {
     public DivisionNode(Node left, Node right) {
         super(left, right, (l,r) ->
             LangFloat.of(Double.parseDouble(l.toString()) / Double.parseDouble(r.toString()))
         );
-    }
-    protected DivisionNode(Node left, Node right, BiFunction<Data, Data, Data> op) {
-        super(left, right, op);
     }
 }
