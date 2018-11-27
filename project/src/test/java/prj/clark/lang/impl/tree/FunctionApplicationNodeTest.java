16ebf17362bf45b544df2aeb39fbe6d9d9b9a222
Matthew Clark <matthew.clark.dev@gmail.com>
2018-11-27 13:29:29 -0600

    Add unit testing for FunctionApplicationNode.

---
+++ project/src/test/java/prj/clark/lang/impl/tree/FunctionApplicationNodeTest.java
@@ -0,0 +1,75 @@
+package prj.clark.lang.impl.tree;
+
+import org.junit.Before;
+import org.junit.Test;
+import prj.clark.lang.impl.env.*;
+import prj.clark.lang.impl.err.FunctionInvocationException;
+import prj.clark.lang.impl.err.LangException;
+import prj.clark.lang.impl.err.TypeMismatchException;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+public class FunctionApplicationNodeTest {
+    private static Function F;
+    private static final StatementListNode TRIPLE_ADDITION;
+
+    static {
+        TRIPLE_ADDITION = new StatementListNode(Collections.singletonList(
+                new AdditionNode(
+                        new IdentifierNode("x"),
+                        new AdditionNode(
+                                new IdentifierNode("y"),
+                                new IdentifierNode("z")
+                        )
+                )
+        ));
+    }
+
+    @Before
+    public void setUp() {
+        F = new GeneratedFunction(TRIPLE_ADDITION, new DefaultContext(), Arrays.asList("x", "y", "z"));
+    }
+
+    private static Node init(Node... args) {
+        return new FunctionApplicationNode(new LiteralNode(F), Arrays.asList(args));
+    }
+
+    @Test
+    public void fullApplicationReturnsCorrectValue() throws LangException {
+        Node n = init(
+                new LiteralNode(LangInt.of(1)),
+                new LiteralNode(LangInt.of(3)),
+                new LiteralNode(LangInt.of(5))
+        );
+
+        assertEquals(LangInt.of(9), n.evaluate(new DummyContext()));
+    }
+
+    @Test
+    public void emptyApplicationReturnsPartialFunction() throws LangException {
+        Node n = init();
+        Data d = n.evaluate(new DummyContext());
+        assertEquals(RawFunction.getInstance(), d.getType());
+    }
+
+    @Test(expected = FunctionInvocationException.class)
+    public void excessApplicationFails() throws LangException {
+        Node n = init(
+                new LiteralNode(LangInt.of(1)),
+                new LiteralNode(LangInt.of(3)),
+                new LiteralNode(LangInt.of(0)),
+                new LiteralNode(LangInt.of(5))
+        );
+
+        n.evaluate(new DummyContext());
+    }
+
+    @Test(expected = TypeMismatchException.class)
+    public void nonFunctionThrowsException() throws LangException {
+        Node n = new FunctionApplicationNode(new LiteralNode(LangString.of("")), Collections.emptyList());
+        n.evaluate(new DummyContext());
+    }
+}
