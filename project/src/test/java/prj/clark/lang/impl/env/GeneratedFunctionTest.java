7f1fb55f8048a8f0406e8919ea6482c1f58bebae
Matthew Clark <matthew.clark.dev@gmail.com>
2018-11-27 12:02:44 -0600

    Add relevant testing for parameter binding and partial application.

--- project/src/test/java/prj/clark/lang/impl/env/GeneratedFunctionTest.java
+++ project/src/test/java/prj/clark/lang/impl/env/GeneratedFunctionTest.java
@@ -16,6 +16,20 @@ import static org.junit.Assert.*;
  * Note that this class tests with the assumption that all functions are raw.
  */
 public class GeneratedFunctionTest {
+    private static final StatementListNode TRIPLE_ADDITION;
+
+    static {
+        TRIPLE_ADDITION = new StatementListNode(Collections.singletonList(
+                new AdditionNode(
+                        new IdentifierNode("x"),
+                        new AdditionNode(
+                                new IdentifierNode("y"),
+                                new IdentifierNode("z")
+                        )
+                )
+        ));
+    }
+
     private class FailingBody extends StatementListNode {
         private FailingBody() {
             super(null);
@@ -101,5 +115,62 @@ public class GeneratedFunctionTest {
         StatementListNode body = new StatementListNode(Collections.emptyList());
         Function f = new GeneratedFunction(body, new DummyContext(), new ArrayList<>());
         assertEquals(Empty.get(), f.apply(Collections.emptyList()));
+    }
+
+    @Test
+    public void functionCorrectlyBindsSingleParameter() throws LangException {
+        StatementListNode body = new StatementListNode(Collections.singletonList(new IdentifierNode("x")));
+        Function f = new GeneratedFunction(body, new DefaultContext(), Collections.singletonList("x"));
+        assertEquals(LangBool.of(false), f.apply(Collections.singletonList(LangBool.of(false))));
+    }
+
+    @Test
+    public void functionCorrectlyBindsMultipleParameters() throws LangException {
+        StatementListNode body = new StatementListNode(
+                Collections.singletonList(
+                        new AdditionNode(
+                                new IdentifierNode("x"),
+                                new IdentifierNode("y")
+                        )
+                )
+        );
+        Function f = new GeneratedFunction(body, new DefaultContext(), Arrays.asList("x", "y"));
+        assertEquals(LangInt.of(12), f.apply(Arrays.asList(LangInt.of(6), LangInt.of(6))));
+    }
+
+    private static Function triple() {
+        return new GeneratedFunction(TRIPLE_ADDITION, new DefaultContext(), Arrays.asList("x", "y", "z"));
+    }
+
+    @Test
+    public void zeroArgumentsRequiresThreeMore() throws LangException {
+        Function f = triple();
+        Data d = f.apply(Collections.emptyList());
+        assertEquals(RawFunction.getInstance(), d.getType());
+        assertEquals(LangInt.of(6), ((Function) d).apply(Arrays.asList(LangInt.of(1), LangInt.of(2), LangInt.of(3))));
+    }
+
+    @Test
+    public void oneArgumentRequiresTwoMore() throws LangException {
+        Function f = triple();
+        Data d = f.apply(Collections.singletonList(LangInt.of(7)));
+        assertEquals(RawFunction.getInstance(), d.getType());
+        assertEquals(LangInt.of(12), ((Function) d).apply(Arrays.asList(LangInt.of(2), LangInt.of(3))));
+    }
+
+    @Test
+    public void twoArgumentsRequiresOneMore() throws LangException {
+        Function f = triple();
+        Data d = f.apply(Arrays.asList(LangInt.of(11), LangInt.of(-1)));
+        assertEquals(RawFunction.getInstance(), d.getType());
+        assertEquals(LangInt.of(14), ((Function) d).apply(Collections.singletonList(LangInt.of(4))));
+    }
+
+    @Test
+    public void fullApplicationDoesNotReturnPartial() throws LangException {
+        Function f = triple();
+        Data d = f.apply(Arrays.asList(LangInt.of(11), LangInt.of(-1), LangInt.of(9)));
+        assertNotEquals(RawFunction.getInstance(), d.getType());
+        assertEquals(LangInt.of(19), d);
     }
 }
