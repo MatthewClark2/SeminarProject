d23854a14579d8564b71572fcc2026fcc12e48fb
Matthew Clark <matthew.clark.dev@gmail.com>
2018-11-27 12:48:10 -0600

    Addressed issue regarding StatementListNode::evaluate.

--- project/src/main/java/prj/clark/lang/impl/tree/StatementListNode.java
+++ project/src/main/java/prj/clark/lang/impl/tree/StatementListNode.java
@@ -2,6 +2,7 @@ package prj.clark.lang.impl.tree;
 
 import prj.clark.lang.impl.env.Context;
 import prj.clark.lang.impl.env.Data;
+import prj.clark.lang.impl.env.Empty;
 import prj.clark.lang.impl.err.LangException;
 
 import java.util.List;
@@ -16,10 +17,11 @@ public class StatementListNode implements Node {
 
     @Override
     public Data evaluate(Context ctx) throws LangException {
-        for (int i = 0; i < statements.size() - 1; ++i) {
-            statements.get(i).evaluate(ctx);
+        Data d = Empty.get();
+        for (int i = 0; i < statements.size(); ++i) {
+            d = statements.get(i).evaluate(ctx);
         }
 
-        return statements.get(statements.size() - 1).evaluate(ctx);
+        return d;
     }
 }
