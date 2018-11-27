1eb718eb9685dc88db516fa12d1ffc144a09b24f
Matthew Clark <matthew.clark.dev@gmail.com>
2018-11-27 12:36:58 -0600

    Add the logic for partial function application.

--- project/src/main/java/prj/clark/lang/impl/env/GeneratedFunction.java
+++ project/src/main/java/prj/clark/lang/impl/env/GeneratedFunction.java
@@ -5,6 +5,8 @@ import prj.clark.lang.impl.err.LangException;
 import prj.clark.lang.impl.tree.StatementListNode;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Used for functions that are created at runtime. This class should conform to the automatic currying in the language.
@@ -16,6 +18,28 @@ public class GeneratedFunction implements Function {
     private final Context enclosingContext;
     private final int argCount;
     private final List<String> arguments;
+
+    // TODO(matthew-c21) - Consider removing this to an external class.
+    private static class PartiallyAppliedFunction implements Function {
+        private final List<Data> suppliedArguments;
+        private final Function baseFunction;
+
+        public PartiallyAppliedFunction(List<Data> suppliedArguments, Function baseFunction) {
+            this.suppliedArguments = suppliedArguments;
+            this.baseFunction = baseFunction;
+        }
+
+        @Override
+        public Data apply(List<Data> args) throws LangException {
+            // TODO(matthew-c21) - Determine whether stream concatenation or the creation of a new list is faster.
+            return baseFunction.apply(Stream.concat(suppliedArguments.stream(), args.stream()).collect(Collectors.toList()));
+        }
+
+        @Override
+        public DataType getType() {
+            return RawFunction.getInstance();
+        }
+    }
 
     /**
      * Create a new GeneratedFunction that may be utilized.
@@ -35,7 +59,7 @@ public class GeneratedFunction implements Function {
     public Data apply(List<Data> args) throws LangException {
         if (args.size() < argCount) {
             // return a partial function.
-            return null;
+            return new PartiallyAppliedFunction(args, this);
         } else if (args.size() > argCount) {
             throw new FunctionInvocationException("Too many arguments supplied to function.");
         } else {
