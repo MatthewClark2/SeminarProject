628bf8a4ffaac19999b150a9f0b4af80d4fd7908
Matthew Clark <matthew.clark.dev@gmail.com>
2018-12-28 16:32:29 -0600

    Add implementation and unit tests for ScopedContext.

---
+++ project/src/main/java/prj/clark/lang/impl/env/ScopedContext.java
@@ -0,0 +1,52 @@
+package prj.clark.lang.impl.env;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Serves as a disposable wrapper for classes that require their own execution contexts, such as {@link Function}s.
+ * Any bindings made through this class do not affect the originally supplied {@link Context}, although changes in the
+ * original may be reflected through the wrapper.
+ */
+public class ScopedContext implements Context {
+    // The outer map contains everything into a module like structure. This is currently unused.
+    private Map<String, Map<String, Data>> mutableBindings;
+    private Map<String, Map<String, Data>> immutableBindings;
+    private Context original;
+    // Consider injecting this.
+    private Context current;
+
+    private static final String MODULE = ".";
+
+    public ScopedContext(Context original) {
+        this.original = original;
+        mutableBindings = new HashMap<>();
+        immutableBindings = new HashMap<>();
+
+        mutableBindings.put(MODULE, new HashMap<>());
+        immutableBindings.put(MODULE, new HashMap<>());
+        current = new DefaultContext();
+    }
+
+    @Override
+    public void bindMutably(String identifier, Data d) {
+        current.bindMutably(identifier, d);
+    }
+
+    @Override
+    public void bindImmutably(String identifier, Data d) {
+        current.bindImmutably(identifier, d);
+    }
+
+    @Override
+    public Optional<Data> search(String identifier) {
+        Optional<Data> originalResult = original.search(identifier);
+
+        if (originalResult.isPresent()) {
+            return originalResult;
+        }
+
+        return current.search(identifier);
+    }
+}
---
+++ project/src/test/java/prj/clark/lang/impl/env/ScopedContextTest.java
@@ -0,0 +1,124 @@
+package prj.clark.lang.impl.env;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.*;
+
+public class ScopedContextTest {
+    private static final Context ORIGINAL;
+    private static Context scoped;
+
+    private static final class FrozenContext implements Context {
+        private Map<String, Data> bindings;
+
+        private FrozenContext(Map<String, Data> bindings) {
+            this.bindings = bindings;
+        }
+
+        @Override
+        public void bindMutably(String identifier, Data d) {
+            fail();
+        }
+
+        @Override
+        public void bindImmutably(String identifier, Data d) {
+            fail();
+        }
+
+        @Override
+        public Optional<Data> search(String identifier) {
+            return bindings.containsKey(identifier) ? Optional.of(bindings.get(identifier)) : Optional.empty();
+        }
+    }
+
+    static {
+        Map<String, Data> data = new HashMap<>();
+        data.put("hello", LangString.of("world"));
+        data.put("foo", LangFloat.of(2.5));
+        data.put("False", LangBool.of(false));
+        ORIGINAL = new FrozenContext(data);
+    }
+
+    @Before
+    public void setUp() {
+        scoped = new ScopedContext(ORIGINAL);
+    }
+
+    @Test
+    public void accessToOriginalBindingsAvailable() {
+        assertTrue(scoped.search("hello").isPresent());
+        assertTrue(scoped.search("foo").isPresent());
+        assertTrue(scoped.search("False").isPresent());
+    }
+
+    @Test
+    public void accessToOriginalBindingsAccurate() {
+        Consumer<String> f = s -> assertEquals(ORIGINAL.search(s).get(), scoped.search(s).get());
+        f.accept("hello");
+        f.accept("foo");
+        f.accept("False");
+    }
+
+    @Test
+    public void unboundVariablesNotAvailable() {
+        assertFalse(scoped.search("bar").isPresent());
+        assertFalse(scoped.search("014lksaf").isPresent());
+        // Smile emoji.
+        assertFalse(scoped.search("\uD83D\uDE0A").isPresent());
+    }
+
+    @Test
+    public void mayBindValues() {
+        scoped.bindMutably("True", LangBool.of(true));
+        scoped.bindImmutably("name", LangString.of("Matthew"));
+    }
+
+    @Test
+    public void mayNotRebindImmutableValuesImmutably() {
+        scoped.bindImmutably("name", LangString.of("Matthew"));
+        scoped.bindImmutably("name", LangString.of("Billy"));
+    }
+
+    @Test
+    public void mayNotRebindImmutableValuesMutably() {
+        scoped.bindImmutably("name", LangString.of("Matthew"));
+        scoped.bindMutably("name", LangString.of("Billy"));
+    }
+
+    @Test
+    public void mayRebindMutableValuesMutably() {
+        scoped.bindMutably("api_endpoint", LangString.of("www.example.com/my-api"));
+        scoped.bindMutably("api_endpoint", LangString.of("www.example.com/my-new-api"));
+    }
+
+    @Test
+    public void mayRebindMutableValuesImmutably() {
+        scoped.bindMutably("api_endpoint", LangString.of("www.example.com/my-api"));
+        scoped.bindImmutably("api_endpoint", LangString.of("www.example.com/my-new-api"));
+    }
+
+    @Test
+    public void boundValuesAccurate() {
+        scoped.bindMutably("n", LangInt.of(7));
+        scoped.bindImmutably("pi", LangFloat.of(Math.PI));
+        assertEquals(LangInt.of(7), scoped.search("n").get());
+        assertEquals(LangFloat.of(Math.PI), scoped.search("pi").get());
+    }
+
+    @Test
+    public void reBoundValuesAccurate() {
+        scoped.bindMutably("n", LangInt.of(7));
+        scoped.bindImmutably("pi", LangFloat.of(Math.PI));
+        assertEquals(LangInt.of(7), scoped.search("n").get());
+        assertEquals(LangFloat.of(Math.PI), scoped.search("pi").get());
+
+        scoped.bindMutably("n", LangInt.of(8));
+        assertEquals(LangInt.of(8), scoped.search("n").get());
+    }
+}
