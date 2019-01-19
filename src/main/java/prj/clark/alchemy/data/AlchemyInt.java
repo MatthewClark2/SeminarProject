39d9fe87ec2ab009a6a055f01659ca4a98aaaf26
Matthew Clark <matthew.clark.dev@gmail.com>
2019-01-19 16:15:43 -0600

    Fix AlchemyInt.

--- src/main/java/prj/clark/alchemy/data/AlchemyInt.java
+++ src/main/java/prj/clark/alchemy/data/AlchemyInt.java
@@ -1,24 +1,62 @@
 package prj.clark.alchemy.data;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  * Contains information for integer values. This may be altered in the future
  * to default to arbitrary precision, or there may be several implementations
  * that work with various levels of precision.
  */
-public class AlchemyInt extends PrimitiveData<Long> {
-    private AlchemyInt(long value) {
-        super(value, PrimitiveType.INT);
+// TODO(matthew-c21) - Determine whether or not both implementations are required.
+public class AlchemyInt implements Numeric, Printable {
+    private final BigDecimal bfValue;
+    private final BigInteger biValue;
+    private final double fValue;
+    private final long iValue;
+
+    private AlchemyInt(long i, double f) {
+        bfValue = new BigDecimal(f);
+        biValue = new BigInteger("" + i);
+        fValue = f;
+        iValue = i;
     }
 
     public static Data of(long value) {
-        return new AlchemyInt(value);
+        return new AlchemyInt(value, (double) value);
     }
 
     public static Data of(String content) {
         try {
-            return new AlchemyInt(Long.parseLong(content));
+            long l = Long.parseLong(content);
+            return of(l);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(e);
         }
+    }
+
+    @Override
+    public BigDecimal arbitraryFloatValue() {
+        return bfValue;
+    }
+
+    @Override
+    public BigInteger arbitraryIntValue() {
+        return biValue;
+    }
+
+    @Override
+    public long intValue() {
+        return iValue;
+    }
+
+    @Override
+    public double floatValue() {
+        return fValue;
+    }
+
+    @Override
+    public String print() {
+        return biValue.toString();
     }
 }
