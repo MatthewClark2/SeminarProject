128dc2817aed57492d0dac35e1c6dfef05cec891
Matthew Clark <matthew.clark.dev@gmail.com>
2019-01-28 22:19:56 -0600

    Update AlchemyBooleanTest to match new API.

--- src/test/java/prj/clark/alchemy/data/AlchemyBooleanTest.java
+++ src/test/java/prj/clark/alchemy/data/AlchemyBooleanTest.java
@@ -7,14 +7,14 @@ import static org.junit.Assert.*;
 public class AlchemyBooleanTest {
     @Test
     public void trueStringCreatesTrue() {
-        Data b = AlchemyBoolean.of("true");
-        assertTrue(((AlchemyBoolean) b).getValue());
+        Data b = AlchemyBoolean.of("True");
+        assertTrue(b.toBoolean());
     }
 
     @Test
     public void falseStringCreatesFalse() {
-        Data b = AlchemyBoolean.of("false");
-        assertFalse(((AlchemyBoolean) b).getValue());
+        Data b = AlchemyBoolean.of("False");
+        assertFalse(b.toBoolean());
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -24,31 +24,31 @@ public class AlchemyBooleanTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void incorrectCasingThrowsException() {
-        Data b = AlchemyBoolean.of("True");
+        Data b = AlchemyBoolean.of("true");
     }
 
     @Test
     public void literalTrueIsTrue() {
         Data b = AlchemyBoolean.of(true);
-        assertTrue(((AlchemyBoolean) b).getValue());
+        assertTrue(b.toBoolean());
     }
 
     @Test
     public void literalFalseIsFalse() {
         Data b = AlchemyBoolean.of(false);
-        assertFalse(((AlchemyBoolean) b).getValue());
+        assertFalse(b.toBoolean());
     }
 
     @Test
     public void trueValuesEqual() {
-        Data a = AlchemyBoolean.of("true");
+        Data a = AlchemyBoolean.of("True");
         Data b = AlchemyBoolean.of(true);
         assertEquals(a, b);
     }
 
     @Test
     public void falseValuesEqual() {
-        Data a = AlchemyBoolean.of("false");
+        Data a = AlchemyBoolean.of("False");
         Data b = AlchemyBoolean.of(false);
         assertEquals(a, b);
     }
@@ -63,24 +63,24 @@ public class AlchemyBooleanTest {
 
     @Test
     public void generatedValuesOfTypeBool() {
-        Data a = AlchemyBoolean.of("true");
+        Data a = AlchemyBoolean.of("True");
         Data b = AlchemyBoolean.of(true);
-        Data c = AlchemyBoolean.of("false");
+        Data c = AlchemyBoolean.of("False");
         Data d = AlchemyBoolean.of(false);
 
-        assertEquals(PrimitiveType.BOOL, a.getType());
-        assertEquals(PrimitiveType.BOOL, b.getType());
-        assertEquals(PrimitiveType.BOOL, c.getType());
-        assertEquals(PrimitiveType.BOOL, d.getType());
+        assertTrue(a instanceof AlchemyBoolean);
+        assertTrue(b instanceof AlchemyBoolean);
+        assertTrue(c instanceof AlchemyBoolean);
+        assertTrue(d instanceof AlchemyBoolean);
     }
 
     @Test
     public void trueToString() {
-
+        assertEquals("True", AlchemyBoolean.TRUE.toString());
     }
 
     @Test
     public void falseToString() {
-
+        assertEquals("False", AlchemyBoolean.FALSE.toString());
     }
 }
