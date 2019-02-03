b09ae76e1a981b6f965e82016b9a96b8af60e852
Matthew Clark <matthew.clark.dev@gmail.com>
2019-02-03 13:23:55 -0600

    Add some implementation detail to AlchemyList.

--- src/main/java/prj/clark/alchemy/data/AlchemyList.java
+++ src/main/java/prj/clark/alchemy/data/AlchemyList.java
@@ -1,5 +1,8 @@
 package prj.clark.alchemy.data;
 
+import prj.clark.alchemy.err.TypeMismatchException;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
 import java.util.Optional;
@@ -8,26 +11,77 @@ public class AlchemyList implements Sequenced, Sliceable, Printable {
     private final List<Data> data;
 
     public AlchemyList(List<Data> data) {
-        this.data = data;
+        this.data = new ArrayList<>(data);
     }
 
     @Override
     public Iterator iter() {
-        return null;
+        return data.iterator();
     }
 
     @Override
     public Sequenced slice(Numeric start, Numeric end, Numeric n) {
+        // TODO(matthew-c21) - Implement using a lazy sequence.
         return null;
     }
 
     @Override
     public Optional<Data> getIndex(Data index) {
-        return Optional.empty();
+        // TODO(matthew-c21) - Fix thrown exceptions.
+        // We specifically check for an AlchemyInt to avoid being able to use floats on accident.
+        if (! (index instanceof AlchemyInt)) {
+            throw new TypeMismatchException();
+        }
+
+        int i = (int) (((Numeric) data).intValue());
+
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return (i < this.data.size())
+                ? Optional.of(this.data.get(i))
+                : Optional.empty();
     }
 
     @Override
     public String print() {
-        return null;
+        StringBuilder sb = new StringBuilder("[");
+
+        for (int i = 0; i < data.size() - 1; ++i) {
+            sb.append(data.get(i));
+            sb.append(", ");
+        }
+
+        sb.append(data.get(data.size() - 1));
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return print();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Sequenced) {
+            Iterator a = ((Sequenced) o).iter();
+            for (Iterator i = iter(); i.hasNext() && a.hasNext();) {
+                if (! a.next().equals(i.next())) {
+                    return false;
+                }
+            }
+
+            return ! a.hasNext();
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return data.hashCode();
     }
 }
