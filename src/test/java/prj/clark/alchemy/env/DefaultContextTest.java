8295c59790600a227c4a7d2000a995c9c6a263fc
Matthew Clark <matthew.clark.dev@gmail.com>
2019-03-14 14:37:53 -0500

    Add non-module related tests for DefaultContext.

--- src/test/java/prj/clark/alchemy/env/DefaultContextTest.java
+++ src/test/java/prj/clark/alchemy/env/DefaultContextTest.java
@@ -2,11 +2,13 @@ package prj.clark.alchemy.env;
 
 import org.junit.Before;
 import org.junit.Test;
-import prj.clark.alchemy.data.AlchemyBoolean;
-import prj.clark.alchemy.data.EagerAlchemyList;
+import prj.clark.alchemy.data.*;
 import prj.clark.alchemy.err.IllegalRebindingException;
 
+import java.util.Collections;
+
 import static org.junit.Assert.*;
+import static prj.clark.alchemy.TestUtils.compareSequences;
 
 public class DefaultContextTest {
     private Context ctx;
@@ -15,23 +17,35 @@ public class DefaultContextTest {
     public void setUp() {
         ctx = new DefaultContext();
         ctx.bind("foo", AlchemyBoolean.TRUE);
-        ctx.bindFunction("i", EagerAlchemyList::new);
+        ctx.bindFunction("i", (args) -> new EagerAlchemyList(Collections.emptyList()));
     }
 
     @Test
-    public void ableToFindBoundValues() {}
+    public void ableToFindBoundValues() {
+        assertTrue(ctx.search("foo").isPresent());
+        assertEquals(AlchemyBoolean.TRUE, ctx.search("foo").get());
+    }
 
     @Test
-    public void ableToFindBoundFunctions() {}
+    public void ableToFindBoundFunctions() {
+        assertTrue(ctx.search("i").isPresent());
+        compareSequences(new EagerAlchemyList(Collections.emptyList()),
+                (Sequenced) ((Invokable) (ctx.search("i").get()))
+                .invoke(Collections.emptyList()));
+    }
 
     @Test
     public void ableToFindBoundModules() {}
 
     @Test
-    public void unableToFindUnboundValues() {}
+    public void unableToFindUnboundValues() {
+        assertFalse(ctx.search("k").isPresent());
+    }
 
     @Test(expected = IllegalRebindingException.class)
-    public void mayNotBindIdUsedForFunction() {}
+    public void mayNotBindIdUsedForFunction() {
+        ctx.bind("i", AlchemyInt.of(3));
+    }
 
     @Test(expected = IllegalRebindingException.class)
     public void mayNotBindIdUsedForModule() {}
