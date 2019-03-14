2a6ce6765d2fb8ecf1e7345d5b1c2dfdb018ccc3
Matthew Clark <matthew.clark.dev@gmail.com>
2019-03-14 00:19:14 -0500

    Update Context to allow for more types of binding.

--- src/main/java/prj/clark/alchemy/env/Context.java
+++ src/main/java/prj/clark/alchemy/env/Context.java
@@ -1,6 +1,7 @@
 package prj.clark.alchemy.env;
 
 import prj.clark.alchemy.data.Data;
+import prj.clark.alchemy.data.Invokable;
 import prj.clark.alchemy.err.IllegalRebindingException;
 
 import java.util.Optional;
@@ -17,9 +18,26 @@ public interface Context {
      * value immutably.
      * @param identifier the identifier being bound.
      * @param d the value being bound to the given identifier.
-     * @throws IllegalRebindingException if the given identifier is already used as an import.
+     * @throws IllegalRebindingException if the given identifier is already used as an import or function.
      */
     void bind(String identifier, Data d);
+
+    /**
+     * Import a new module into the current context. Since Modules don't exist yet, this definition is incomplete. Check
+     * this method again in the future for futher details.
+     * @throws IllegalRebindingException if the given identifier is already used as an import or function.
+     */
+    // TODO(matthew-c21) - Should module evaluation be done here or elsewhere?
+    default void importModule() {}
+
+    /**
+     * Bind a function in the current context. The identifier should be unused, and may not be rebound later in the
+     * given context.
+     * @param identifier the name to be bound to the function.
+     * @param i the function being bound.
+     * @throws IllegalRebindingException if the given identifier is already used as an import or function.
+     */
+    default void bindFunction(String identifier, Invokable i) {}
 
     /**
      * Determine if a given identifier has been bound within the current {@link Context}.
