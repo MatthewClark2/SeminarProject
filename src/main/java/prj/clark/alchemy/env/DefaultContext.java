ce2713ede65407d133587eddfcc8944a66247fd7
Matthew Clark <matthew.clark.dev@gmail.com>
2019-03-14 00:27:30 -0500

    Update definition of DefaultContext.

--- src/main/java/prj/clark/alchemy/env/DefaultContext.java
+++ src/main/java/prj/clark/alchemy/env/DefaultContext.java
@@ -1,53 +1,59 @@
 package prj.clark.alchemy.env;
 
 import prj.clark.alchemy.data.Data;
+import prj.clark.alchemy.data.Invokable;
 import prj.clark.alchemy.err.IllegalRebindingException;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import static prj.clark.alchemy.validation.IdentifierValidation.validate;
-
+/**
+ * This class serves as the de facto implementation for {@link Context}, and is safe to use as a basis for other
+ * contexts.
+ */
 public class DefaultContext implements Context {
-    // The outer map contains everything into a module like structure. This is currently unused.
-    // TODO(matthew-c21) - Decide on how to manage where bindings originate.
-    private Map<String, Map<String, Data>> mutableBindings;
-    private Map<String, Map<String, Data>> immutableBindings;
-
-    private static final String MODULE = ".";
+    private Map<String, Data> basicBindings;
+    private Map<String, Data> functions;
+    private Map<String, Data> imports;
 
     public DefaultContext() {
-        // TODO(matthew-c21) - Prepopulate these with any global language constants.
-        mutableBindings = new HashMap<>();
-        immutableBindings = new HashMap<>();
-
-        mutableBindings.put(MODULE, new HashMap<>());
-        immutableBindings.put(MODULE, new HashMap<>());
+        // TODO(matthew-c21) - Since this is the "default", prepopulate it.
+        basicBindings = new HashMap<>();
+        functions = new HashMap<>();
+        imports = new HashMap<>();
     }
 
     @Override
     public void bind(String identifier, Data d) {
-        // TODO(matthew-c21) - Decide on whether or not another level of abstraction would be useful here to avoid
-        // direct validation.
-        if (immutableBindings.get(MODULE).containsKey(identifier)) {
-            throw new IllegalRebindingException();
-        }
+        ensureUnused(identifier);
 
-        validate(identifier);
-        mutableBindings.get(MODULE).put(identifier, d);
+        basicBindings.put(identifier, d);
+    }
+
+    @Override
+    public void bindFunction(String identifier, Invokable i) {
+        ensureUnused(identifier);
+
+        functions.put(identifier, i);
     }
 
     @Override
     public Optional<Data> search(String identifier) {
-        if (immutableBindings.get(MODULE).containsKey(identifier)) {
-            return Optional.of(immutableBindings.get(MODULE).get(identifier));
-        }
-
-        if (mutableBindings.get(MODULE).containsKey(identifier)) {
-            return Optional.of(mutableBindings.get(MODULE).get(identifier));
+        if (basicBindings.containsKey(identifier)) {
+            return Optional.of(basicBindings.get(identifier));
+        } else if (functions.containsKey(identifier)) {
+            return Optional.of(functions.get(identifier));
+        } else if (imports.containsKey(identifier)) {
+            return Optional.of(imports.get(identifier));
         }
 
         return Optional.empty();
+    }
+
+    private void ensureUnused(String identifier) {
+        if (functions.containsKey(identifier) || imports.containsKey(identifier)) {
+            throw new IllegalRebindingException(identifier + " already in use.");
+        }
     }
 }
