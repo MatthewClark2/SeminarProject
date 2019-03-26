6508a5cd378460a08a9e5339a10a72904c33353a
Matthew Clark <matthew.clark.dev@gmail.com>
2019-03-26 18:17:01 -0500

    Add test stubs to AlchemyRange.

---
+++ src/test/java/prj/clark/alchemy/data/AlchemyRangeTest.java
@@ -0,0 +1,45 @@
+package prj.clark.alchemy.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlchemyRangeTest {
+    @Test
+    public void toTen() {
+
+    }
+
+    @Test
+    public void toTwentiesEven() {}
+
+    @Test
+    public void fullySetRange() {}
+
+    @Test
+    public void printAndToStringSame() {}
+
+    @Test
+    public void printBehavesCorrectly() {}
+
+    @Test
+    public void infiniteRangeHoldsAtLeast2e64Elements() {}
+
+    @Test
+    public void sliceBehavesCorrectly() {}
+
+    @Test
+    public void negativeIndexThrowsException() {}
+
+    @Test
+    public void floatIndexThrowsException() {}
+
+    @Test
+    public void floatingPointRange() {}
+
+    @Test
+    public void stopLessThanStartYieldsNoElements() {}
+
+    @Test
+    public void negativeRange() {}
+}
