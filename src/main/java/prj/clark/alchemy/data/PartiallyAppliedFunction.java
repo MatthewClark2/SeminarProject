25cb48540a7d05b34b0bf54f51fb708904037732
Matthew Clark <matthew.clark.dev@gmail.com>
2019-03-29 19:52:31 -0500

    Update PartiallyAppliedFunction to keep track of trailing arguments as well as leading supplied ones.

--- src/main/java/prj/clark/alchemy/data/PartiallyAppliedFunction.java
+++ src/main/java/prj/clark/alchemy/data/PartiallyAppliedFunction.java
@@ -1,26 +1,50 @@
 package prj.clark.alchemy.data;
 
+import prj.clark.alchemy.err.FunctionInvocationException;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PartiallyAppliedFunction implements Invokable {
-    private final List<Data> suppliedArguments;
+    private final List<Data> leadingArguments;
     private final Invokable baseFunction;
+    private final int totalSuppliedArguments;
+    private final List<Data> trailingArguments;
 
-    public PartiallyAppliedFunction(List<Data> suppliedArguments, Invokable baseFunction) {
-        this.suppliedArguments = suppliedArguments;
+    public PartiallyAppliedFunction(List<Data> leadingArguments, Invokable baseFunction) {
+        this(leadingArguments, new ArrayList<>(), baseFunction);
+    }
+
+    public PartiallyAppliedFunction(List<Data> leadingArguments, List<Data> trailingArguments, Invokable baseFunction) {
+        this.leadingArguments = leadingArguments;
+        this.trailingArguments = trailingArguments;
         this.baseFunction = baseFunction;
+        this.totalSuppliedArguments = leadingArguments.size() + trailingArguments.size();
+        if (totalSuppliedArguments > baseFunction.parameterCount()) {
+            throw new IllegalArgumentException("Too many total parameters supplied for given function.");
+        }
     }
 
     @Override
     public Data invoke(List<Data> args) {
-        // TODO(matthew-c21) - Determine whether stream concatenation or the creation of a new list is faster.
-        return baseFunction.invoke(Stream.concat(suppliedArguments.stream(), args.stream()).collect(Collectors.toList()));
+        int argCount = totalSuppliedArguments + args.size();
+
+        if (argCount > baseFunction.parameterCount()) {
+            throw new FunctionInvocationException();
+        } else if (argCount < baseFunction.parameterCount()) {
+            List<Data> newLeadingArguments = new ArrayList<>(leadingArguments);
+            newLeadingArguments.addAll(args);
+            return new PartiallyAppliedFunction(newLeadingArguments, trailingArguments, baseFunction);
+        } else {
+            List<Data> invokingArgs = new ArrayList<>(leadingArguments);
+            invokingArgs.addAll(args);
+            invokingArgs.addAll(trailingArguments);
+            return baseFunction.invoke(invokingArgs);
+        }
     }
 
     @Override
     public int parameterCount() {
-        return 0;
+        return baseFunction.parameterCount() - totalSuppliedArguments;
     }
 }
