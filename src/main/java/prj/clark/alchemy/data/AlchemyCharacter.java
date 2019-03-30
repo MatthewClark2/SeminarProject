733ac2b1e21d254ac4d2b1f6a845593facb60592
Matthew Clark <matthew.clark.dev@gmail.com>
2019-03-30 13:12:59 -0500

    Add String constructor to AlchemyCharacter to run new unit tests.

--- src/main/java/prj/clark/alchemy/data/AlchemyCharacter.java
+++ src/main/java/prj/clark/alchemy/data/AlchemyCharacter.java
@@ -1,11 +1,30 @@
 package prj.clark.alchemy.data;
 
+import prj.clark.alchemy.err.StringFormatException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class is used to wrap UTF-16 characters as Alchemy {@link Data}.
  *
  * Characters are always considered truthy.
  */
 public class AlchemyCharacter implements Printable {
+    private static final Map<Character, Character> ESCAPE_MAP;
+
+    static {
+        ESCAPE_MAP = new HashMap<>();
+        ESCAPE_MAP.put('r', '\r');
+        ESCAPE_MAP.put('f', '\f');
+        ESCAPE_MAP.put('t', '\t');
+        ESCAPE_MAP.put('n', '\n');
+        ESCAPE_MAP.put('\\', '\\');
+        ESCAPE_MAP.put('"', '"');
+        ESCAPE_MAP.put('\'', '\'');
+        ESCAPE_MAP.put('b', '\b');
+    }
+
     private final int codePoint;
     // We hold this for caching purposes.
     private final String representation;
@@ -25,6 +44,32 @@ public class AlchemyCharacter implements Printable {
 
     public static AlchemyCharacter of(byte b) {
         return new AlchemyCharacter(b);
+    }
+
+    public static AlchemyCharacter of(String value) {
+        if (value.length() == 0) {
+            throw new StringFormatException("Cannot make character from empty string.");
+        }
+
+        if (value.length() == 1) {
+            return AlchemyCharacter.of(value.charAt(0));
+        }
+
+        if (value.matches("^\\\\.$")) {
+            // The string is just a single escape.
+            char next = value.charAt(1);
+
+            if (ESCAPE_MAP.containsKey(next)) {
+                return new AlchemyCharacter(ESCAPE_MAP.get(next));
+            } else {
+                throw new StringFormatException("Unrecognized escape character: " + next);
+            }
+        } else if (value.matches("^\\\\u[a-fA-F0-9]{4}$")) {
+            String hexCode = value.substring(2);
+            return AlchemyCharacter.of(Integer.parseInt(hexCode, 16));
+        } else {
+            throw new StringFormatException("Unrecognized character format.");
+        }
     }
 
     @Override
