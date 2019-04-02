11b29f8a9d32d3f44a29dccd0a86df94facc945d
Matthew Clark <matthew.clark.dev@gmail.com>
2019-04-02 17:30:50 -0500

    Implement string concatenation.

--- src/main/java/prj/clark/alchemy/tree/StringConcatenationNode.java
+++ src/main/java/prj/clark/alchemy/tree/StringConcatenationNode.java
@@ -1,16 +1,28 @@
 package prj.clark.alchemy.tree;
 
+import prj.clark.alchemy.data.AlchemyString;
 import prj.clark.alchemy.data.Data;
 import prj.clark.alchemy.env.Context;
+import prj.clark.alchemy.err.TypeMismatchException;
 
 public class StringConcatenationNode extends ReferentiallyTransparentValuedNode {
+    private final Valued left;
+    private final Valued right;
 
     public StringConcatenationNode(Valued left, Valued right) {
-
+        this.left = left;
+        this.right = right;
     }
 
     @Override
     public Data evaluate(Context ctx) {
-        return null;
+        Data l = left.evaluate(ctx);
+        Data r = right.evaluate(ctx);
+
+        if (! (l instanceof AlchemyString && r instanceof AlchemyString)) {
+            throw new TypeMismatchException();
+        }
+
+        return AlchemyString.of(l.toString() + r.toString());
     }
 }
